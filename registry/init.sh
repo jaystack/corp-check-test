93333cd9a3087e5fe1fac9bf13752398070524ce
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-10-31 11:36:18 +0100

    wip

--- registry/init.sh
+++ registry/init.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+echo "INIT REGISTRY"
+
 CWD=$(dirname $0)
 
 echo "NPM_CONFIG_REGISTRY: $NPM_CONFIG_REGISTRY"
+
+echo "CREATE NPM USER"
 
 npm adduser << EOF
 test
 test
 test@test.test
 EOF
+
+echo "PUBLISH MOCK NPM PACKAGES"
 
 npm publish "$CWD/packages/a"
 npm publish "$CWD/packages/b"
