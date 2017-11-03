974e7d12ea459a1288685db572516328bff4dcf5
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-11-03 10:06:28 +0100

    wip

--- test/popular-packages.e2e.ts
+++ test/popular-packages.e2e.ts
@@ -11,7 +11,31 @@ describe('popular-packages', () => {
   it('are exist', async () => {
     let result = [];
     while (result.length < 3) result = await api.get('popular-packages');
-    console.log(result);
-    expect(result).toEqual([]);
+    
+    expect(result).toHaveLength(3);
+
+    expect(result[0]).toHaveProperty('cid');
+    expect(result[0]).toHaveProperty('name', 'popular-a');
+    expect(result[0]).toHaveProperty('version', '1.0.0');
+    expect(result[0]).toHaveProperty('state.type', 'SUCCEEDED');
+    expect(result[0]).toHaveProperty('state.date');
+    expect(result[0]).toHaveProperty('qualification', 'RECOMMENDED');
+    expect(result[0]).toHaveProperty('score', 0.9);
+
+    expect(result[1]).toHaveProperty('cid');
+    expect(result[1]).toHaveProperty('name', 'popular-b');
+    expect(result[1]).toHaveProperty('version', '1.0.0');
+    expect(result[1]).toHaveProperty('state.type', 'SUCCEEDED');
+    expect(result[1]).toHaveProperty('state.date');
+    expect(result[1]).toHaveProperty('qualification', 'ACCEPTED');
+    expect(result[1]).toHaveProperty('score', 0.4);
+
+    expect(result[2]).toHaveProperty('cid');
+    expect(result[2]).toHaveProperty('name', 'popular-c');
+    expect(result[2]).toHaveProperty('version', '1.0.0');
+    expect(result[2]).toHaveProperty('state.type', 'SUCCEEDED');
+    expect(result[2]).toHaveProperty('state.date');
+    expect(result[2]).toHaveProperty('qualification', 'REJECTED');
+    expect(result[2]).toHaveProperty('score', 0);
   });
 });
